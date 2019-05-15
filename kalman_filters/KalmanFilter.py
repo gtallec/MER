8c81112cd6b5138c5e6ea99ed7c9624bc6a835f5
gtallec <gauthier.tallec996@gmail.com>
2019-05-15 20:36:01 +0200

    send code to server

---
+++ kalman_filters/KalmanFilter.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class KalmanFilter:
+
+    def __init__(self, save_path):
+
+        self.save_path
+
+        self.F = None
+        self.Q = None
+        self.G = None
+        self.R = None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    kalman_filter = KalmanFilter()
+
