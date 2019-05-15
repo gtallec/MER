7066667cbee5936c24fea6274f86caac95fdd745
gtallec <gauthier.tallec996@gmail.com>
2019-05-15 21:47:27 +0200

    send code to server

--- kalman_filters/KalmanFilter.py
+++ kalman_filters/KalmanFilter.py
@@ -39,7 +39,7 @@ class KalmanFilter:
         y : observation time serie.
 
         """
-        N, M, D  = A.shape + (0, 1, 0)
+        N, M - 1, D  = A.shape
 
         inter_1 = (np.einsum('nik,nkj->ij',
                              A,
